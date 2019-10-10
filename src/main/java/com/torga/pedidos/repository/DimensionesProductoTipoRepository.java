671d7ced97042214fa19d0b9a2b8c051daee2126
jose99 <jose45335@gmail.com>
2019-10-10 08:37:53 +0200

    order by consultas

--- src/main/java/com/torga/pedidos/repository/DimensionesProductoTipoRepository.java
+++ src/main/java/com/torga/pedidos/repository/DimensionesProductoTipoRepository.java
@@ -22,15 +22,16 @@ public interface DimensionesProductoTipoRepository extends JpaRepository<Dimensi
 	@Query("SELECT u FROM DimensionesProductoTipo u WHERE u.id = ?1")
 	Collection<DimensionesProductoTipo> findDimensiones(Long id);
 	
-	@Query("SELECT u.alto, u.ancho, u.fondo, u.precio, u.productosDormitorio, u.id FROM DimensionesProductoTipo u WHERE u.productosDormitorio.categoriasDormi.id = ?1 and u.ancho = ?2")
+	@Query("SELECT u.alto, u.ancho, u.fondo, u.precio, u.productosDormitorio, u.id FROM DimensionesProductoTipo u WHERE u.productosDormitorio.categoriasDormi.id = ?1 and u.ancho = ?2 order by u.productosDormitorio.nombre")
 	Collection<DimensionesProductoTipo> findFiltro(Long id, Float ancho);
 	
-	@Query("SELECT u.alto, u.ancho, u.fondo, u.precio, u.productosDormitorio, u.id FROM DimensionesProductoTipo u WHERE u.productosDormitorio.categoriasDormi.id = ?1 and u.alto = ?2")
+	@Query("SELECT u.alto, u.ancho, u.fondo, u.precio, u.productosDormitorio, u.id FROM DimensionesProductoTipo u WHERE u.productosDormitorio.categoriasDormi.id = ?1 and u.alto = ?2 order by u.productosDormitorio.nombre")
 	Collection<DimensionesProductoTipo> findFiltroAltura(Long id, Float altura);
 	
-	@Query("SELECT u.alto, u.ancho, u.fondo, u.precio, u.productosDormitorio, u.id FROM DimensionesProductoTipo u WHERE u.productosDormitorio.categoriasDormi.id = ?1 and u.alto = ?2 and u.ancho = ?3")
+	@Query("SELECT u.alto, u.ancho, u.fondo, u.precio, u.productosDormitorio, u.id FROM DimensionesProductoTipo u WHERE u.productosDormitorio.categoriasDormi.id = ?1 and u.alto = ?2 and u.ancho = ?3 order by u.productosDormitorio.nombre")
 	Collection<DimensionesProductoTipo> findFiltroAlturaAncho(Long id, Float altura, Float ancho);
 	
-	@Query("SELECT u.alto, u.ancho, u.fondo, u.precio, u.productosDormitorio, u.id FROM DimensionesProductoTipo u WHERE u.productosDormitorio.categoriasDormi.id = ?1 and u.fondo = ?2")
+	@Query("SELECT u.alto, u.ancho, u.fondo, u.precio, u.productosDormitorio, u.id FROM DimensionesProductoTipo u WHERE u.productosDormitorio.categoriasDormi.id = ?1 and u.fondo = ?2 order by u.productosDormitorio.nombre")
 	Collection<DimensionesProductoTipo> findFiltroFondo(Long id, Float fondo);
 }
+ 
