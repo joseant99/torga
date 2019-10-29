61a06b7e5e9f36a2fd0eb5b535546b4decd287ad
jose99 <jose45335@gmail.com>
2019-10-29 11:53:05 +0100

    error solucionado

--- src/main/webapp/app/entities/presupuesto-pedido/index.ts
+++ src/main/webapp/app/entities/presupuesto-pedido/index.ts
@@ -6,5 +6,6 @@ export * from './presupuesto-pedido.component';
 export * from './presupuesto-edicion.component';
 export * from './presupuesto-usuario.component';
 export * from './pedidos-usuario.component';
+export * from './pedidos-productos.component';
 export * from './presupuesto-pedido.route';
 export * from './presupuesto-productos.component';
