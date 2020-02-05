27dccdb40de10e52dacf036caf0dc5a8b403defa
jose99 <jose45335@gmail.com>
2020-02-05 12:25:48 +0100

    cambios corregidos 100%

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -464,7 +464,7 @@ export class NavbarComponent implements AfterViewInit, OnInit {
                                         this.subscribeToSaveResponse1(
                                             this.productosPresupuestoPedidosService.create(this.productosPresupuestoPedidos)
                                         );
-                                        console.log(this.productosPresupuestoPedidos);
+                                        alert(this.productosPresupuestoPedidos['presupuestoPedido']['id']);
 
                                         if (numeroAcaProd[m].length != 0) {
                                             var acaPedProd = this.acaProdPed.length;
